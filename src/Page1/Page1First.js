6451f0591ee8c639cfbb2eabed8e2c60c525c3d8
parkjunho2 <pp01024689860@gmail.com>
2025-03-04 12:28:39 +0900

    edit

--- src/Page1/Page1First.js
+++ src/Page1/Page1First.js
@@ -10,11 +10,11 @@ const Page1First=()=>{
       useEffect(() => {
             new TypeIt("#title", {
               speed: 50,
-            }).pause(1000).go();
+            }).pause(500).go();
           
             new TypeIt("#title2", {
               speed: 150,
-            }).pause(1000).go();
+            }).pause(500).go();
             
             setTimeout(() => {
                   ScrollOut({
@@ -33,8 +33,8 @@ const Page1First=()=>{
     return(<>
           <div className='row w-100'>
             <div className='col-md-6 col-sm-12 ps-5'>
-                  <h2 id='title'>Don’t reinvent the wheel, just improve it</h2>
-                  <h2 className="mb-5" id='title2'>바퀴를 발명하지말고, 개선해나가자</h2>
+                  <h2 id='title'>Don’t reinvent the wheel, just improve it!</h2>
+                  <h2 className="mb-5" id='title2'>바퀴를 발명하지말고, 개선해나가자!</h2>
                   <h4 data-scroll2>Email: pp01024689860@gmail.com</h4>
                   <a href="https://github.com/parkjunho2">
                   <h2 className="my-5 text-black" title="GitHub" data-scroll2><FaGithub /></h2>      
