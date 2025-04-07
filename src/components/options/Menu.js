cce7e077eb908108c1cf68c39560f7ccfb161c5f
parkjunho2 <pp01024689860@gmail.com>
2025-04-07 19:04:18 +0900

    update

--- src/components/options/Menu.js
+++ src/components/options/Menu.js
@@ -10,7 +10,7 @@ const Menu=()=>{
             <a href="https://host.sysout.co.kr/kh14sc/home/login" className='menu-item'>EC5 JQuery DNS</a>
           </li>
           <li data-menuanchor="React">
-            <a href="https://topguntravel.shop" className='menu-item'>EC6 React DNS</a>
+            <a href="https://homeweb.kro.kr/" className='menu-item'>EC6 React DNS</a>
           </li>
           <li data-menuanchor="Extensions">
             <a href="#Extensions" className='menu-item'>Extensions DNS </a>
